fcca7b994d99d3297aa5b3cdedf7654523a3deff
Leela-Prasad <leela.jagu@gmail.com>
2024-08-31 14:04:05 +0530

    Create Reducer

---
+++ redux-starter/src/reducer.js
@@ -0,0 +1,19 @@
+let lastId = 0;
+
+function reducer(state = [], dispatch) {
+
+    if(dispatch.type === "bugAdded") {
+        return [
+            ...state,
+            {
+                id: ++lastId,
+                description: dispatch.payload.description,
+                resolved: false
+            }
+        ]
+    } else if(dispatch.type === "bugRemoved") {
+        return state.filter(bug => bug.id !== dispatch.payload.id)
+    } 
+
+    return state;
+}
