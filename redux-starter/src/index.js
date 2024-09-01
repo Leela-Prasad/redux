b15bf384b833d3544643e71e81b3be8fd4079009
Leela-Prasad <leela.jagu@gmail.com>
2024-09-01 11:33:11 +0530

    Dispatching Actions

---
+++ redux-starter/src/index.js
@@ -0,0 +1,23 @@
+import store from "./store"
+
+console.log(store);
+console.log(store.getState());
+
+
+store.dispatch({
+    type: "bugAdded",
+    payload: {
+        description: "Bug1"
+    }
+})
+
+console.log(store.getState());
+
+store.dispatch({
+    type: "bugRemoved",
+    payload: {
+        id: 1
+    }
+})
+
+console.log(store.getState());
