4e7e5b798c7b89467da2a94acab6eadb268ce638
Leela-Prasad <leela.jagu@gmail.com>
2024-09-05 13:11:21 +0530

    createSlice using redux toolkit

--- redux-starter/src/store/bugs.js
+++ redux-starter/src/store/bugs.js
@@ -1,38 +1,31 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Action Creators
-// bugAdded is a function which returns object with type "bugAdded" and payload with undefined
-// to form the payload we have to pass object or primitive to this function 
-// bugAdded -> {type: "bugAdded", payload: undefined}
-// bugAdded({description: "Bug 1"}) -> {type: "bugAdded", payload: {description: "Bug 1"}}
-export const bugAdded = createAction("bugAdded")
-export const bugRemoved = createAction("bugRemoved")
-export const bugResolved = createAction("bugResolved")
-
-// Reducer
 let lastId = 0;
 
-export default createReducer([], {
-    //key: value
-    //action: functions similar to event => event handler
-
-    [bugAdded.type] : (bugs, action) => {
-        //Here we are mutating just like a javascript way
-        //but behind the scenes it uses Immer to create an Immutable object
-        bugs.push({
-            id: ++lastId,
-            description: action.payload.description,
-            resolved: false
-        })
-    },
-
-    [bugRemoved.type]: (bugs, action) => {
-        const index = bugs.findIndex(bug => bug.id === action.payload.id)
-        bugs.splice(index, 1)
-    },
-
-    [bugResolved.type]: (bugs, action) => {
-        const index = bugs.findIndex(bug => bug.id === action.payload.id)
-        bugs[index].resolved = true
+const slice = createSlice({
+    name: "bugs",
+    initialState: [],
+    reducers: {
+        //actions => action handlers
+        bugAdded: (bugs, action) => {
+            bugs.push({
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false
+            })
+        },
+        bugRemoved: (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id)
+            bugs.splice(index, 1)
+        },
+        bugResolved: (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id)
+            bugs[index].resolved = true
+        }
     }
 })
+
+console.log(slice);
+
+export const {bugAdded, bugRemoved, bugResolved} = slice.actions
+export default slice.reducer
