7971141a9765ffa6623256042c9ed2545d41cca7
Leela-Prasad <leela.jagu@gmail.com>
2024-09-06 15:17:55 +0530

    Dispatching Functions using Thunk

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
 import func from "./middleware/func";
@@ -6,10 +6,9 @@ import func from "./middleware/func";
 export default function configureAppStore() {
     return configureStore({ 
         reducer,
-        // middleware: [logger("Console")]
         middleware: [
-            logger({destination: "Console"}),
-            func
+            ...getDefaultMiddleware(),
+            logger({destination: "Console"})
         ]
 
     });
