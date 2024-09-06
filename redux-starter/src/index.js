f25a7a0906137966a3c64f151ecf969be5a3111e
Leela-Prasad <leela.jagu@gmail.com>
2024-09-06 14:57:29 +0530

    Dispatching Functions to make Async API Calls This is done without Thunk

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,35 +1,12 @@
 import configureAppStore from "./store/configureStore";
-import { projectAdded } from "./store/projects";
-import {bugAdded, bugRemoved, bugResolved, bugAssignedToUser, getUnresolvedBugs, getBugsByUserId} from "./store/bugs"
-import { userAdded } from "./store/users";
 
 const store = configureAppStore()
-
 store.state = []
-// console.log(store);
 
-store.subscribe(() => console.log("Store Changed", store.getState()))
-
-store.dispatch(bugAdded({description: "Bug1"}))
-// store.dispatch(bugAdded({description: "Bug2"}))
-// store.dispatch(bugAdded({description: "Bug3"}))
-// // store.dispatch(bugRemoved({id: 3}))
-// store.dispatch(bugResolved({id: 1}))
-
-// store.dispatch(projectAdded({name: "Project 1"}))
-
-// const x = getUnresolvedBugs(store.getState())
-// const y = getUnresolvedBugs(store.getState())
-
-// console.log(x === y);
-
-// store.dispatch(userAdded({name: "Leela"}))
-// store.dispatch(userAdded({name: "John"}))
-
-// store.dispatch(bugAssignedToUser({bugId: 1, userId: 1}))
-// store.dispatch(bugAssignedToUser({bugId: 3, userId: 1}))
-
-// const bugs = getBugsByUserId(1)(store.getState())
-
-// console.log(bugs);
+store.dispatch((dispatch, getState) => {
+    // Call an API
+    // When the promise is resolved => dispatch(result)
+    // If the promise is rejected => dispatch(error)
+    dispatch({type: "bugsReceived", bugs: [1, 2, 3]})
+})
 
--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
+import func from "./middleware/func";
 
 export default function configureAppStore() {
     return configureStore({ 
         reducer,
         // middleware: [logger("Console")]
-        middleware: [logger({destination: "Console"})]
+        middleware: [
+            logger({destination: "Console"}),
+            func
+        ]
 
     });
 }
