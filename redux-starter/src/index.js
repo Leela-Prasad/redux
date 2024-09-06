945609dcfe58f684d8117b3e44cad31b0f34643b
Leela-Prasad <leela.jagu@gmail.com>
2024-09-06 16:55:26 +0530

    Exercise: Dispatch Error Messages for Toast Notification

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -3,10 +3,17 @@ import configureAppStore from "./store/configureStore";
 const store = configureAppStore()
 store.state = []
 
-store.dispatch((dispatch, getState) => {
-    // Call an API
-    // When the promise is resolved => dispatch(result)
-    // If the promise is rejected => dispatch(error)
-    dispatch({type: "bugsReceived", bugs: [1, 2, 3]})
+// store.dispatch((dispatch, getState) => {
+//     // Call an API
+//     // When the promise is resolved => dispatch(result)
+//     // If the promise is rejected => dispatch(error)
+//     dispatch({type: "bugsReceived", bugs: [1, 2, 3]})
+// })
+
+store.dispatch({
+    type: "error",
+    payload: {
+        message: "An error occurred"
+    }
 })
 
--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -2,13 +2,15 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
 import func from "./middleware/func";
+import toast from "./middleware/toast";
 
 export default function configureAppStore() {
     return configureStore({ 
         reducer,
         middleware: [
             ...getDefaultMiddleware(),
-            logger({destination: "Console"})
+            logger({destination: "Console"}),
+            toast
         ]
 
     });
