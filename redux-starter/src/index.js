916b67fb7f01f6429104e11371799cea47f00c79
Leela-Prasad <leela.jagu@gmail.com>
2024-09-07 15:00:46 +0530

    Refactor Bugs Slice and dispatch method

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,27 +1,8 @@
-import * as actions from "./store/api";
+import { loadBugs } from "./store/bugs";
 import configureAppStore from "./store/configureStore";
 
 const store = configureAppStore()
 store.state = []
 
-// store.dispatch((dispatch, getState) => {
-//     // Call an API
-//     // When the promise is resolved => dispatch(result)
-//     // If the promise is rejected => dispatch(error)
-//     dispatch({type: "bugsReceived", bugs: [1, 2, 3]})
-// })
-
-// store.dispatch({
-//     type: "error",
-//     payload: {
-//         message: "An error occurred"
-//     }
-// })
-
-store.dispatch(
-    actions.apiCallBegan({
-        url: "/bugs",
-        onSuccess: "bugsReceived"
-    })
-)
+store.dispatch(loadBugs())
 
